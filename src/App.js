2d48d79175b9177d2c3e07fd144000f493343744
superbeom <it.super.beom@gmail.com>
2020-09-21 17:10:33 +0900

    Add Login page

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 import Header from "./Header";
 import Home from "./Home";
 import Checkout from "./Checkout";
+import Login from "./Login";
 
 function App() {
   return (
@@ -12,6 +13,9 @@ function App() {
         <Header />
 
         <Switch>
+          <Route path="/login">
+            <Login />
+          </Route>
           <Route path="/checkout">
             <Checkout />
           </Route>
